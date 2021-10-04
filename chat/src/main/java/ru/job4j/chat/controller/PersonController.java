6b38eaf1981dd06c05db0f2c8e9f693d7afb5db8
Roman Pogorelov <rmk140987@gmail.com>
2021-10-04 09:20:58 +0300

    5. Обработка исключений и Spring REST [#504797]

--- chat/src/main/java/ru/job4j/chat/controller/PersonController.java
+++ chat/src/main/java/ru/job4j/chat/controller/PersonController.java
@@ -1,12 +1,18 @@
 package ru.job4j.chat.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import ru.job4j.chat.model.Person;
 import ru.job4j.chat.repository.PersonRepository;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -22,6 +28,14 @@ public class PersonController {
         this.encoder = encoder;
     }
 
+    @GetMapping("/{id}")
+    public Person findById(@PathVariable long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "Person is not found. Please, check id."
+                ));
+    }
+
     @GetMapping("/")
     public List<Person> findAll() {
         return StreamSupport.stream(
@@ -31,8 +45,28 @@ public class PersonController {
 
     @PostMapping("/")
     public ResponseEntity<Person> add(@RequestBody Person person) {
-        person.setPassword(encoder.encode(person.getPassword()));
+        String name = person.getName();
+        String password = person.getPassword();
+        if (name == null || password == null) {
+            throw new NullPointerException("Username and password mustn't be empty");
+        }
+        if (password.length() < 6) {
+            throw new IllegalArgumentException("Invalid password. Password length must be more than 5 characters.");
+        }
+        person.setPassword(encoder.encode(password));
         return new ResponseEntity<>(this.repository.save(person), HttpStatus.CREATED);
+    }
+
+    @ExceptionHandler(value = {IllegalArgumentException.class})
+    public void exceptionHandler(Exception e, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
+        response.setContentType("application/json");
+        response.getWriter().write(new ObjectMapper().writeValueAsString(new HashMap<>() {
+            {
+                put("message", e.getMessage());
+                put("type", e.getClass());
+            }
+        }));
     }
 
     @PutMapping("/")
